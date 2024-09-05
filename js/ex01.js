4c7acde1a4daf253d0ba32c83866d99702f21914
SuzuMiyaYuuGi <115784306+SuzuMiyaYuuGi@users.noreply.github.com>
2024-09-05 15:12:47 +0700

    Delete js directory

--- js/ex01.js
+++
@@ -1,6 +0,0 @@
-const matches = (obj,source) => Object.keys(source).every(key => obj.hasOwnProperty(key)&&obj[key]===source[key]);
-console.log(matches({age:25,hair:'long',beard:true},{hair:'long',beard:true}));
-console.log(matches({hair:'long',beard:true},{age:25,hair:'long',beard:true}));
-document.getElementById("matches1").innerHTML = matches({age:25,hair:'long',beard:true},{hair:'long',beard:true});
-document.getElementById("matches2").innerHTML = matches({hair:'long',beard:true},{age:25,hair:'long',beard:true});
-
--- js/script.js
+++
@@ -1,6 +0,0 @@
-console.log('this is my script file form javascript');
-console.log('hello javascript');
-console.log('hello Thursday');
-document.getElementById("text").innerHTML = "Ug Kongthong";
-document.write('Hello document write');
-window.alert('welcome to my page');
