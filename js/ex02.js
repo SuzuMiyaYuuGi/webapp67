2ef18bb07917703b539e2b1bc5a6bb40dad47525
SuzuMiyaYuuGi <115784306+SuzuMiyaYuuGi@users.noreply.github.com>
2024-09-05 15:22:48 +0700

    Delete js directory

--- js/ex02.js
+++
@@ -1,9 +0,0 @@
-const random_Number_in_Range = (min,max) => Math.random()*(max-min)+min;
-
-const result1 = random_Number_in_Range(2, 10);
-const result2 = random_Number_in_Range(1, 5);
-console.log(result1);
-console.log(result2);
-
-document.getElementById("ex02res1").innerHTML = result1;
-document.getElementById("ex02res2").innerHTML = result2;
--- js/ex03.js
+++
@@ -1,7 +0,0 @@
-const digitize = n =>[...`${n}`].map(i =>parseInt(i));
-
-console.log(digitize(123));
-console.log(digitize(1230));
-
-document.getElementById("123").innerHTML = digitize(123);
-document.getElementById("1230").innerHTML = digitize(1230);
